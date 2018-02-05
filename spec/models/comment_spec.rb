0f68f8c0d92b7c176e42e0bf520e145ec426d12c
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-04 22:29:37 -0700

    Add model specs for relationships. Create Comment model. Add relationships to comment and article models. Passing tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,6 +4,8 @@ describe Comment, type: :model do
   describe "validations" do
     it {should validate_presence_of(:author_name)}
     it {should validate_presence_of(:body)}
-    it {should belong_to(:article)}
   end
+  describe "relationships" do
+    it {should belong_to(:article)}
+  end 
 end
