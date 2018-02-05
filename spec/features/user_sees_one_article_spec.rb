d1486d7fd381d0722a5fd45418c9a97e4a736177
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-04 22:53:12 -0700

    Add spec for comments. Add comment to show template. Create comment partial

--- spec/features/user_sees_one_article_spec.rb
+++ spec/features/user_sees_one_article_spec.rb
@@ -4,13 +4,20 @@ describe "user sees one article" do
   describe "user links from the articles index" do
     it "sees information for one artcle" do
       article = Article.create!(title: "Title 1", body: "Body 1")
+      comment_1 = article.comments.create(author_name: "Me", body: "Comment comments")
+      comment_2 = article.comments.create(author_name: "You", body: "So much to say")
 
-      visit '/articles'
+
+      visit articles_path
       click_link article.title
 
       expect(page).to have_content(article.title)
       expect(page).to have_content(article.body)
       expect(current_path).to eq("/articles/#{article.id}")
+      expect(page).to have_content(comment_1.author_name)
+      expect(page).to have_content(comment_1.body)
+      expect(page).to have_content(comment_2.author_name)
+      expect(page).to have_content(comment_2.body)
     end
   end
 end
